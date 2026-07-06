4ee699dfc8b713820782e5955d806d45a524f236
Dam <damianotrovato122@gmail.com>
2026-07-06 14:19:50 +0200

    new: loading configurations from csv files

--- src/naive_parallel.c
+++ src/naive_parallel.c
@@ -1,11 +1,14 @@
 #include "./headers/ocl_boiler.h"
 #include "./headers/sim-utils.h"
-#include "./headers/n-body-init.h"
 #include <sys/stat.h>
+#include <limits.h>
 
 #define DELTA_TIME 0.02f
 #define CENTER_DISTANCE 10
+#define GALAXIES_PATH "./galaxies/"
+#define OUTPUTS_PATH "./outputs/"
 #define SEED 42
+
 
 
 cl_event update_force_run_k(
@@ -143,8 +146,8 @@ cl_event update_vel_run_k(
 
 int main(int argc, char *argv[]) {
     
-    if (argc < 4) {
-        printf("correct usage: %s, [body count], [iterations], [simulation-name]\n", argv[0]);
+    if (argc < 5) {
+        printf("correct usage: %s, [body count], [iterations], [config-name], [simulation-name]\n", argv[0]);
         return EXIT_FAILURE;
     }
 
@@ -160,7 +163,8 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    char *sim_name = argv[3];
+    char *galaxy_name = argv[3];
+    char *sim_name = argv[4];
 
     /*openCL shenanigans*/
     cl_platform_id p = select_platform();
@@ -198,6 +202,39 @@ int main(int argc, char *argv[]) {
     size_t body_force_buffer_size = sizeof(cl_float2) * body_count;
     size_t body_mass_buffer_size = sizeof(cl_float) * body_count;
 
+    /*READING THE CONFIGURATION*/
+    char galaxy_path_name[1024] = GALAXIES_PATH;
+    strcat(galaxy_path_name, galaxy_name);
+
+    FILE *fp = fopen(galaxy_path_name, "r");
+    if (fp == NULL) {
+        perror("error reading the file");
+        return EXIT_FAILURE;
+    }
+    printf("file opened succesfully\n");
+
+    char line[128];
+    float X, Y, vX, vY, mass;
+
+    int row = 0;
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        if (sscanf(line, "%f,%f,%f,%f,%f", &X, &Y, &vX, &vY, &mass) == 5) {
+            body_pos[row].x = X;
+            body_pos[row].y = Y;
+            body_vel[row].x = vX;
+            body_vel[row].y = vY;
+            body_force[row].x = 0;
+            body_force[row].y = 0;
+            body_mass[row] = mass;
+        } else {
+            fprintf(stderr, "error reading the row no.\n%s", line);
+        }
+        row++;
+    }
+
+    fclose(fp);
+    printf("file closed succesfully.\n");
+
     cl_mem body_pos_mem = clCreateBuffer(
         ctx,
         CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
@@ -234,6 +271,11 @@ int main(int argc, char *argv[]) {
     );
     ocl_check(err, "clCreateBuffer failed on body_mass");
 
+    free(body_pos);
+    free(body_vel);
+    free(body_force);
+    free(body_mass);
+
     cl_event update_force_event[iterations + 1], update_pos_event[iterations], update_vel_event[iterations + 1];
 
     update_force_event[0] = update_force_run_k(
@@ -260,9 +302,9 @@ int main(int argc, char *argv[]) {
     clWaitForEvents(1, update_vel_event);
 
     cl_event enqueue_map_buffer_event;
-    char path_name[1024] = "./outputs/";
-    strcat(path_name, sim_name);
-    mkdir(path_name, S_IRWXU);
+    char outputs_path_name[1024] = OUTPUTS_PATH;
+    strcat(outputs_path_name, sim_name);
+    mkdir(outputs_path_name, S_IRWXU);
 
     for (int i = 0; i < iterations; i++) {
         update_pos_event[i] = update_pos_run_k(
@@ -312,7 +354,7 @@ int main(int argc, char *argv[]) {
             &err
         );
         ocl_check(err, "enqueueMapBufferEvent failed");
-
+        
         body_vel = clEnqueueMapBuffer(
             que, 
             body_vel_mem, 
@@ -327,8 +369,8 @@ int main(int argc, char *argv[]) {
         );
 
         ocl_check(err, "enqueueMapBufferEvent failed");
-
-        //write_frame_on_disk(body_count, bodies, sim_name, i);
+        
+        write_frame_on_disk(body_count, body_pos, sim_name, i);
 
         cl_event enqueue_unmap_event;
         err = clEnqueueUnmapMemObject(
@@ -352,11 +394,11 @@ int main(int argc, char *argv[]) {
     printf("TIMES:\n\nupdate_pos: %gms,\nupdate_vel: %gms,\nupdate_force: %gms,\nenqueue_map_buffer: %gms\n",
     time_pos_ms, time_vel_ms, time_force_ms, time_enqueue_map_ms);
     
-    
-    for (int i = 0; i < 10; i++) {
-        printf("%f %f\n", body_pos[i].x, body_pos[i].y);
-    }
     /*
+    for (int i = 0; i < 100; i++) {
+        printf("%f %f\n", body_pos[i].x, body_pos[i].y);
+    }*/
+    
     clReleaseMemObject(body_pos_mem);
     clReleaseMemObject(body_vel_mem);
     clReleaseMemObject(body_force_mem);
@@ -365,14 +407,10 @@ int main(int argc, char *argv[]) {
     clReleaseKernel(update_force_k);
     clReleaseKernel(update_pos_k);
     clReleaseKernel(update_vel_k);
-
+    
     clReleaseProgram(prog);
     clReleaseCommandQueue(que);
     clReleaseContext(ctx);
     
-    free(body_pos);
-    free(body_vel);
-    free(body_force);
-    free(body_mass);
-    */
+    
 }
