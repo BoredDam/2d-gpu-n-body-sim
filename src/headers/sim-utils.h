a1e7c2082362972e4786794425de91dbf5435688
Dam <damianotrovato122@gmail.com>
2026-07-18 17:07:05 +0200

    feat: function to write bounding box coordinates and size on disk

--- src/headers/sim-utils.h
+++ src/headers/sim-utils.h
@@ -19,3 +19,20 @@ void write_frame_on_disk(const int count, const cl_float2 *bodies, const char *s
     }
     fclose(fp); 
 }
+
+
+void write_bounding_box_on_disk(const int count, const cl_float2 *centers, const cl_float *half_size, const char *sim_name, const int time) {
+    /* 
+    another shitty function to handle writing frames on disk:/
+    */
+    FILE *fp;
+    char file_name[512];
+    sprintf(file_name, "./outputs/%s/%s_frame_%d.csv", sim_name, sim_name, time);
+    fp = fopen(file_name, "w+");
+
+    fprintf(fp, "x,y,r\n");
+    for (int i = 0; i < count; i++) {
+        fprintf(fp, "%f,%f,%f\n", centers[i].x, centers[i].y, half_size[i]);
+    }
+    fclose(fp); 
+}
